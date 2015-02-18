8f38287f9eb7d3218dd37fa2b58ba19a347e8906
frankito9999 <perfumefrankito@hotmail.com>
2015-02-18 18:37:51 -0400

    modificacion del controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -49,17 +49,22 @@ app.controller("homeController", function($scope,$http)
     $scope.abrirModal = function(size){
        
 
-        var modalInstance = $modal.open({
-          templateUrl: 'myModalContent.html',
-          controller: 'ModalInstanceCtrl',
-          size: size,
-          resolve: {
-            items: function () {
-              return $scope.items;
-            }
-          }
+        // var modalInstance = $modal.open({
+        //   templateUrl: '../partials/modal.html',
+        //   controller: 'ModalInstanceCtrl',
+        //   size: size,
+        //   resolve: {
+        //     items: function () {
+        //       return $scope.items;
+        //     }
+        //   }
 
-        });
+        // });
+        // modalInstance.result.then(function (selectedItem) {
+        //   $scope.selected = selectedItem;
+        // }, function () {
+        //   $log.info('Modal dismissed at: ' + new Date());
+        // });
     };
     $scope.editarCliente = function(cliente){
 
@@ -75,6 +80,7 @@ app.controller("homeController", function($scope,$http)
 
 app.controller('ModalInstanceCtrl', function($scope, $http){
 
+    alert("jiji");
 
 });
 
