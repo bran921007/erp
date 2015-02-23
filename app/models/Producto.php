cea3dfb77a92ef064fda92a92dc77a0002f61a57
Jhonnatan Rodriguez <jhonn@outlook.com>
2015-02-23 01:30:50 -0400

    Update Producto.php

--- app/models/Producto.php
+++ app/models/Producto.php
@@ -2,6 +2,6 @@
 
 class Producto extends \Eloquent {
 	protected $table = 'productos';
-	protected $fillable = ['id',"articulo","cantidad","distribuidor","fecha","tiempo",'created_at','updated_at'];
+	protected $fillable = ['id',"articulo","cantidad","distribuidor","precioVenta","precioCompra","vencimiento","fecha","tiempo",'created_at','updated_at'];
 }
 
