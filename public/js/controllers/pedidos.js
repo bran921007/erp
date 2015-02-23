5955bc09b38ccfc1ec6adc88057ded53a099d6e8
Jhonnatan Rodriguez <jhonn@outlook.com>
2015-02-23 01:24:31 -0400

    Update pedidos.js

--- public/js/controllers/pedidos.js
+++ public/js/controllers/pedidos.js
@@ -2,13 +2,34 @@
 
 	var app = angular.module('pedidos',[]);
 
-	app.controller("pedidosController", function($scope,$http,$log){
+	app.controller("pedidosController", function($scope,$http,$log,$filter){
+
+
+		$scope.realizarPedidoModal = function(){
+			$scope.pedidoModal = !$scope.pedidoModal;
+		}
 
 		$scope.inventario = {};
 			$http.get('/getProductos').success(function(data)
 		{
 			$scope.inventario = data.productos;//así enviamos los posts a la vista
 		});
+
+
+		$scope.crearPedido = function(){
+			var pedido = {
+				id_cliente: $scope.id_cliente,
+				metodo: $scope.metodo,
+				subtotal: $scope.subtotal,
+				itbis: $scope.itbis,
+				total: $scope.total,
+				estado: $scope.estado,
+				tipo: $scope.tipo,
+				fecha: $scope.fecha
+			};
+			$http.post('/postPedido', pedido);
+			$scope.pedidoModal = !$scope.pedidoModal;
+		};
 
 		$scope.hover = function(p) {
 			// Shows/hides the delete button on hover
@@ -20,13 +41,23 @@
 		$scope.itbis = 0;
 		$scope.total = 0;
 
+		$scope.fecha = $filter('date')(Date.now(), "dd-MM-yyyy") ;
+
 		$scope.addCarrito = function(articulo){
+
 			var producto;
 			for (var i = 0; i < $scope.carrito.length; i++) {
 				if ($scope.carrito[i].id == articulo.id) {
 					producto = $scope.carrito[i];
 				}
 			}
+
+			for (var i = 0; i < $scope.inventario.length; i++) {
+				if ($scope.inventario[i].id == articulo.id) {
+					$scope.inventario[i].cantidad--;
+				}
+			}
+
 
 			if (!producto) {
 				$scope.carrito.push({
@@ -40,17 +71,36 @@
 			}
 			// $scope.carrito.push(producto);
 			// console.log($scope.carrito);
+			// $scope.subtotal = $scope.subtotal + articulo.precioVenta;
+			// $scope.itbis = $scope.subtotal * 18 / 100;
+			// $scope.total = $scope.itbis + $scope.subtotal;
 
-			$scope.subtotal = $scope.subtotal + articulo.precioVenta;
-			$scope.itbis = $scope.subtotal * 18 / 100;
-			$scope.total = $scope.itbis + $scope.subtotal;
 		};
+
+		$scope.subtotales = function(){
+			var subtotales = 0;
+			angular.forEach($scope.carrito, function(item){
+				subtotales += item.cantidad * item.precio;
+			});
+
+			return subtotales;
+		};
+
+
+
+
 
 		$scope.deleteArticulo = function(articulo){
 			var producto;
 			for (var i = 0; i < $scope.carrito.length; i++) {
 				if ($scope.carrito[i].id == articulo.id) {
 					producto = $scope.carrito[i];
+				}
+			}
+
+			for (var i = 0; i < $scope.inventario.length; i++) {
+				if ($scope.inventario[i].id == articulo.id) {
+					$scope.inventario[i].cantidad++;
 				}
 			}
 
